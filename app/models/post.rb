78ccf162f9f2d601713a093160e77b4ac70c7da6
Carley Tripp <carley.tripp@outlook.com>
2019-02-14 22:04:19 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,6 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :clickbait_title
 
-  private
-
   def clickbait_title
     clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
 
