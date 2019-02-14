aee6becceeb62f493071ecffceb55ee07714e0c6
Carley Tripp <carley.tripp@outlook.com>
2019-02-14 21:24:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_title
+
+  def clickbait_title
+
+  end
 end
