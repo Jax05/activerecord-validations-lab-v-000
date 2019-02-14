78d0293fc91be097e1fa54f736ce2cddbfa45b0e
Carley Tripp <carley.tripp@outlook.com>
2019-02-14 22:39:21 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   def clickbait_title
     clickbait = /Won't\sBelieve|Secret|Top\s\d|Guess/i
 
-    if title && title !~ clickbait
+    if title !~ clickbait
       errors.add(:title, 'must include clickbait')
     end
   end
